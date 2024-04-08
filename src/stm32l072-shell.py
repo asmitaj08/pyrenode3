f4a688d9416c420794542dd717e5c8a6b9746953
asmitaj08 <asmita.jha82@gmail.com>
2024-04-08 10:59:17 -0700

    test chnages in src/stm32l072-shell.py

--- src/stm32l072-shell.py
+++ src/stm32l072-shell.py
@@ -6,6 +6,8 @@ from Antmicro.Renode.Peripherals.CPU import TranslationCPUHooksExtensions
 e = Emulation()
 m = Monitor()
 
+state_file = "/home/asmita/fuzzing_bare-metal/SEFF_project_dirs/SEFF-project/pyrenode3/src/statefile.dat"
+log_file = "/home/asmita/fuzzing_bare-metal/SEFF_project_dirs/SEFF-project/pyrenode3/src/log.txt"
 # stm32l072 = e.add_mach("stm32l072")
 # stm32l072.load_repl("platforms/cpus/stm32l072.repl")
 
@@ -20,15 +22,25 @@ m = Monitor()
 
 # Tetsing for nrf52840.resc
 
-nrf52840 = e.add_mach("nrf52840")
-nrf52840.load_repl("platforms/cpus/nrf52840.repl")
-nrf52840.load_elf("https://dl.antmicro.com/projects/renode/renode-nrf52840-zephyr_shell_module.elf-gf8d05cf-s_1310072-c00fbffd6b65c6238877c4fe52e8228c2a38bf1f")
-# nrf52840.sysbus.cpu.SetHookAtBlockBegin(my_action)
-TranslationCPUHooksExtensions.SetHookAtBlockBegin(nrf52840.sysbus.cpu.internal, nrf52840.internal, "print('hello world')")
-Analyzer(nrf52840.sysbus.uart0).Show()
+# m.execute(f"logFile @{log_file}")
+# nrf52840 = e.add_mach("nrf52840")
+# nrf52840.load_repl("platforms/cpus/nrf52840.repl")
+# nrf52840.load_elf("https://dl.antmicro.com/projects/renode/renode-nrf52840-zephyr_shell_module.elf-gf8d05cf-s_1310072-c00fbffd6b65c6238877c4fe52e8228c2a38bf1f")
+# # nrf52840.sysbus.cpu.SetHookAtBlockBegin(my_action)
+# TranslationCPUHooksExtensions.SetHookAtBlockBegin(nrf52840.sysbus.cpu.internal, nrf52840.internal, "print 'hello'")
+# Analyzer(nrf52840.sysbus.uart0).Show()
+# e.StartAll()
+# m.execute(f"Save @{state_file}")
+
+# m.execute("Clear")
+# m.execute(f"logFile @{log_file}")
+m.execute(f"Load @{state_file}")
+nrf52840 = e.get_mach("nrf52840")
+m.execute(f"logFile @{log_file}")
 e.StartAll()
+
+# Analyzer(nrf52840.sysbus.uart0).Show()
 m.execute("sysbus.uart0 WriteChar 0x44")
-m.execute("sysbus.uart0 WriteChar 0x46")
 
 
 input()
